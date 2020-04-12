0a4d3e6a3e8d30d7a28595b77c55810b6b578416
Nicholas Keller <nic.keller328@gmail.com>
2020-04-12 21:44:18 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -124,8 +124,7 @@ class MusicLibraryController
 
       input = gets.strip.to_i
       #if (input > 0) && (input <= list_of_songs.size)
-      #if (input >= 1) && (input <= list_of_songs.size)
-      if (1..Song.all.length).include?(input)
+      if (input >= 1) && (input <= list_of_songs.size)
         song = list_of_songs[input+2]
         puts "Playing #{song.name} by #{song.artist.name}"
       end

5b2b1e215a1e0d01f3d3d7a566602b5e6a829c2e
Nicholas Keller <nic.keller328@gmail.com>
2020-04-12 20:53:07 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -68,6 +68,13 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.chomp
+    if Artist.find_by_name(input)
+      sorted_songs = Artist.find_by_name(input).songs.sort_by do |song|
+        song.name
+      end
+      sorted_songs.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
   end
 
   def list_songs_by_genre

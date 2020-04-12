d8cf649ad948b21d1a65d1c894cee3d149ba5f3a
Nicholas Keller <nic.keller328@gmail.com>
2020-04-12 22:05:07 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -111,7 +111,7 @@ class MusicLibraryController
 
       input = gets.strip.to_i
       if (input >= 1) && (input <= song_list.size)
-        song_choice = song_list[input]
+        song_choice = song_list[input+2]
         puts "Playing #{song_choice.name} by #{song_choice.artist.name}"
       end
   end

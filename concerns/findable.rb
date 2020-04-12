6592b03e620ce01420db8af5439f0847ca52f4af
Nicholas Keller <nic.keller328@gmail.com>
2020-04-12 19:00:58 +0000

    Automatically backed up by Learn

--- concerns/findable.rb
+++ concerns/findable.rb
@@ -1,17 +1,17 @@
-module Concerns::Findable
-
-  def find_by_name(object_name)
-    all.find do |object|
-      object.name == object_name
-    end
-  end
-
-  def find_or_create_by_name(object_name)
-    if find_by_name(object_name)
-      find_by_name(object_name)
-    else
-      self.create(object_name)
-    end
-  end
-
-end
+# module Concerns::Findable
+#
+#   def find_by_name(object_name)
+#     all.find do |object|
+#       object.name == object_name
+#     end
+#   end
+#
+#   def find_or_create_by_name(object_name)
+#     if find_by_name(object_name)
+#       find_by_name(object_name)
+#     else
+#       self.create(object_name)
+#     end
+#   end
+#
+# end

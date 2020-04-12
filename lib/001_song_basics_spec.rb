2e3c066f0a4cf6b0ce111f63ad38e8ed0f479f98
Nicholas Keller <nic.keller328@gmail.com>
2020-04-12 20:05:10 +0000

    Automatically backed up by Learn

--- lib/001_song_basics_spec.rb
+++ lib/001_song_basics_spec.rb
@@ -80,7 +80,7 @@ class Song
       genre_obj = Genre.find_or_create_by_name(file_array[2])
       song_obj = self.find_or_create_by_name(file_array[1])
       song_obj.artist = artist_obj
-      song_obj.genre - genre_obj
+      song_obj.genre = genre_obj
       song
   end
 

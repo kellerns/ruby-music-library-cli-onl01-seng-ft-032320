cd397738e1db13968d22cf7e81467c07ee321716
Nicholas Keller <nic.keller328@gmail.com>
2020-04-12 19:28:15 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -4,7 +4,12 @@ class MusicImporter
     @path = path
   end
 
-
+  def files
+    @file = Dir.glob("#{@path}/*.mp3")
+    @file.map do |file_str|
+      file_str.gsub("#{@path}/", "")
+    end
+  end
 
 
 end

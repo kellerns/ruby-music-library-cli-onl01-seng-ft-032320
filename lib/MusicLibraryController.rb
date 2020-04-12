610bed07c40e6e66049f8d47ef48891dd4f9c051
Nicholas Keller <nic.keller328@gmail.com>
2020-04-12 22:34:24 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -74,7 +74,7 @@ class MusicLibraryController
       found_artist = Artist.find_by_name(input)
       sorted_songs = found_artist.songs.sort{ |a, b| a.name <=> b.name }
       sorted_songs.uniq.each.with_index(1) do |song, index|
-        print "#{index}. #{song.name} - #{song.genre.name}"
+        puts "#{index}. #{song.name} - #{song.genre.name}"
       end
     end
   end

62d0c6a0781342075a96d309a4527bc4cf57a5b7
Nicholas Keller <nic.keller328@gmail.com>
2020-04-12 17:22:38 +0000

    Automatically backed up by Learn

--- lib/001_artist_basics_spec.rb
+++ lib/001_artist_basics_spec.rb
@@ -1,5 +1,28 @@
 def Artist
+  attr_accessor :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create(name)
+    self.new(name)
+    save
+  end
+
+  def self.destroy_all
+    @@all.clear_all
+  end
+
+  def save
+    @@all << self
+  end
 
 
 
